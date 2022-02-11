2f2882f0041314a4dd8fa5e5cc923e9c7a40ddef
Ryan Zhang <65325330+irrelevantRyan@users.noreply.github.com>
2022-02-11 15:40:46 -0500

    Update

--- Software_coding/Hough_Art/main.py
+++ Software_coding/Hough_Art/main.py
@@ -1,9 +1,25 @@
+import math
 
-
+# Program Constants
+COL = 300
+ROW = 200
+H_COL = 800
+H_ROW = 800
 
 in_file_name = input("What is the file name:" )
-with open("img.ppm", "r") as in_file:
+with open(in_file_name, "r") as in_file:
+    file_type = next(in_file)
+    width = next(in_file)
+    height = next(in_file)
+    color = next(in_file)
+
+    in_img = []
     for row in in_file:
+        in_img.append([int(i) for  i in row.split()])
+        in_img = in_img[3-1::3]
+
+print(in_img[0])
+print(file_type)
         
 
     
