45935e3043e6212d90155ab0dcbaab72ba741010
irrelevantRyan <65325330+irrelevantRyan@users.noreply.github.com>
2022-05-15 15:32:45 -0400

    Update

--- Unit7[Perceptron]/RyanZhang2024&DanielHalpern2024/Python/Perceptron_ALGO/classification.py
+++ Unit7[Perceptron]/RyanZhang2024&DanielHalpern2024/Python/Perceptron_ALGO/classification.py
@@ -18,7 +18,7 @@ def normalize_sat(sat, max_sat, min_sat):
     :param min_sat: minimum SAT score
     :return: normalized SAT score
     """
-    return (10*((sat - min_sat) / (max_sat - min_sat)))
+    return (10*((sat - 1200) / (1600 - 1200)))
 
 def calculateOutput(weights, sat, gpa, essay, rec, extra):
     """
@@ -33,7 +33,7 @@ def calculateOutput(weights, sat, gpa, essay, rec, extra):
     """
 
     sum =  weights[1] * sat + weights[2] * gpa + weights[3] * essay + weights[4] * rec + weights[5] * extra + weights[0]
-    if sum > 0:
+    if sum >= 0:
         return 1, sum
     else:
         return -1, sum
