f99396d7ca62ddd26b471aa707c126d3198ca75f
Ryan Zhang <82973812+daniel-halpern@users.noreply.github.com>
2022-06-03 13:08:00 -0400

    Base files

---
+++ Unit7-LinearRegression/Regression_ALGO/deployment.py
@@ -0,0 +1,110 @@
+def normalize_gpa(gpa):
+    """
+    Normalize the gpa score
+    :param gpa: gpa score
+    :return: normalized gpa score
+    """
+    return (gpa) * 2.5
+
+def normalize_sat(sat, max_sat, min_sat):
+    """
+    Normalize the SAT score
+    :param sat: SAT score
+    :param max_sat: maximum SAT score
+    :param min_sat: minimum SAT score
+    :return: normalized SAT score
+    """
+    return (10*((sat - 1200) / (1600 - 1200)))
+
+def unnormalize_gpa(gpa):
+    """
+    Unnormalize the gpa score
+    :param gpa: gpa score
+    :return: unnormalized gpa score
+    """
+    return (gpa) / 2.5
+
+def unnormalize_sat(sat):
+    """
+    Unnormalize the SAT score
+    :param sat: SAT score
+    :param max_sat: maximum SAT score
+    :param min_sat: minimum SAT score
+    :return: unnormalized SAT score
+    """
+    return (1200 + (1600 - 1200) * sat / 10)
+
+def calculateOutput(weights, sat, gpa, essay, rec, extra):
+    """
+    Calculate the output of the perceptron
+    :param weights: weights of the perceptron
+    :param sat: SAT score
+    :param gpa: GPA score
+    :param essay: essay score
+    :param rec: recommendation score
+    :param extra: extra score
+    :return: the output of the perceptron
+    """
+
+    sum =  weights[1] * sat + weights[2] * gpa + weights[3] * essay + weights[4] * rec + weights[5] * extra + weights[0]
+    if sum >= 0:
+        return 1, sum
+    else:
+        return -1, sum
+
+
+########################################################################################################################
+#                                                                                                                      # 
+#                                               APP START                                                              #
+#                                                                                                                      #
+########################################################################################################################
+
+
+# Data
+sat = []
+gpa = []
+essay = []
+rec = []
+extra = []
+
+# most recent run
+with open(f'./outputs/weights.txt', 'r') as f:
+    for row in f:
+        weights = row.split()
+        weights = [i.replace(',', '') for i in weights]
+        weights = [float(i) for i in weights]
+        
+# Read the data from the file
+with open('./inputs/application.txt', 'r') as f:
+    for row in f:
+            row_lst = row.split()
+            sat.append(float(row_lst[0]))
+            gpa.append(float(row_lst[1]))
+            essay.append(float(row_lst[2]))
+            rec.append(float(row_lst[3]))
+            extra.append(float(row_lst[4]))
+
+
+# Normalize the sat score
+for i in range(0, len(sat)):
+    sat[i] = normalize_sat(sat[i], max(sat), min(sat))
+
+# Normalize the gpa score
+for i in range(0, len(gpa)):
+    gpa[i] = normalize_gpa(gpa[i])
+
+# Calculate the output
+val = []
+for i in range(0, len(sat)):
+    out, error = calculateOutput(weights, sat[i], gpa[i], essay[i], rec[i], extra[i])
+    val.append(out)
+
+with open(f'./outputs/deploy_output.txt', 'w') as f:
+    for i in range(0, len(val)):
+        if val[i] == 1:
+            val[i] = 'A'
+        else:
+            val[i] = 'R'
+        f.write(f"{i}) SAT: {unnormalize_sat(sat[i])} GPA: {unnormalize_gpa(gpa[i])} Essay: {essay[i]} Rec: {rec[i]} ExtraC: {extra[i]} Prediction: {val[i]}\n")
+        print(f"{i}) SAT: {unnormalize_sat(sat[i])} GPA: {unnormalize_gpa(gpa[i])} Essay: {essay[i]} Rec: {rec[i]} ExtraC: {extra[i]} Prediction: {val[i]}")
+    
---
+++ Unit7-LinearRegression/Regression_ALGO/training.py
@@ -0,0 +1,167 @@
+from random import randint, random
+
+# Plotting libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+def normalize_gpa(gpa):
+    """
+    Normalize the gpa score
+    :param gpa: gpa score
+    :return: normalized gpa score
+    """
+    return (gpa) * 2.5
+
+def normalize_sat(sat, max_sat, min_sat):
+    """
+    Normalize the SAT score
+    :param sat: SAT score
+    :param max_sat: maximum SAT score
+    :param min_sat: minimum SAT score
+    :return: normalized SAT score
+    """
+    return (10*((sat - 1200) / (1600 - 1200)))
+
+def calculateOutput(weights, sat, gpa, essay, rec, extra):
+    """
+    Calculate the output of the perceptron
+    :param weights: weights of the perceptron
+    :param sat: SAT score
+    :param gpa: GPA score
+    :param essay: essay score
+    :param rec: recommendation score
+    :param extra: extra score
+    :return: the output of the perceptron
+    """
+    sum = weights[1] * sat + weights[2] * gpa + weights[3] * essay + weights[4] * rec + weights[5] * extra + weights[0]
+    if sum > 0:
+        return 1
+    else:
+        return -1
+
+
+########################################################################################################################
+#                                                                                                                      # 
+#                                               APP START                                                              #
+#                                                                                                                      #
+########################################################################################################################
+
+
+# Training Data
+sat = []
+gpa = []
+essay = []
+rec = []
+extra = []
+result = []
+
+# Weights
+weights = [random()*10, random()*10, random()*10, random()*10, random()*10, random()*10]
+startingWeights = weights
+
+# Read the data from the file
+with open('./inputs/training.txt', 'r') as f:
+    for row in f:
+            row_lst = row.split()
+            sat.append(float(row_lst[0]))
+            gpa.append(float(row_lst[1]))
+            essay.append(float(row_lst[2]))
+            rec.append(float(row_lst[3]))
+            extra.append(float(row_lst[4]))
+            if row_lst[5] == 'A':
+                result.append(1)
+            else:
+                result.append(-1)
+
+# Normalize the sat score
+for i in range(0, len(sat)):
+    sat[i] = normalize_sat(sat[i], max(sat), min(sat))
+
+# Normalize the gpa score
+for i in range(0, len(gpa)):
+    gpa[i] = normalize_gpa(gpa[i])
+
+print("Initial Weights: w1 = {}, w2 = {}, w3 = {}, w4 = {}, w5 = {}, w0(bias) = {} \n".format(weights[1], weights[2], weights[3], weights[4], weights[5], weights[0]))
+
+learning_rate = 0.0005
+max_epoch = 1000000
+errorList = []
+epochs = []
+trainingOut_list = []
+epoch = 0
+while True:
+    epoch += 1
+    r = randint(0, len(result)-1)
+    if calculateOutput(weights, sat[r], gpa[r], essay[r], rec[r], extra[r]) > result[r]:
+        weights[1] -= learning_rate * sat[r]
+        weights[2] -= learning_rate * gpa[r]
+        weights[3] -= learning_rate * essay[r]
+        weights[4] -= learning_rate * rec[r]
+        weights[5] -= learning_rate * extra[r]
+        weights[0] -= learning_rate
+    elif calculateOutput(weights, sat[r], gpa[r], essay[r], rec[r], extra[r]) < result[r]:
+        weights[1] += learning_rate * sat[r]
+        weights[2] += learning_rate * gpa[r]
+        weights[3] += learning_rate * essay[r]
+        weights[4] += learning_rate * rec[r]
+        weights[5] += learning_rate * extra[r]
+        weights[0] += learning_rate
+
+    error = 0
+    for i in range(0, len(result)):
+        if result[i] != calculateOutput(weights, sat[i], gpa[i], essay[i], rec[i], extra[i]):
+            error += 1
+
+    # For graphs    
+    errorList.append(100*(error/len(result)))
+    epochs.append(epoch)
+    
+    # Print each epoch
+    print("-----------------------------------------------------")
+    print("Epoch {}:".format(epoch))
+    print("Weights: w1 = {}, w2 = {}, w3 = {}, w4 = {}, w5 = {}, w0(bias) = {}".format(weights[1], weights[2], weights[3], weights[4], weights[5], weights[0]))
+    print("-----------------------------------------------------")
+    trainingOut_list.append(f"Iteration: {epoch} Equation {weights[1]}*s + {weights[2]}*g + {weights[3]}*e + {weights[4]}*r + {weights[5]}*c + {weights[0]} = 0 \tERR ="+ str(100*(error/len(result)))+ "%")
+
+
+    if epoch > max_epoch:
+        break
+
+
+with open(f'./outputs/weights.txt', 'w') as f:
+    f.write("{}, {}, {}, {}, {}, {}".format(weights[0], weights[1], weights[2], weights[3], weights[4], weights[5]))
+
+with open(f'./outputs/errors.txt', 'w') as f:
+    for i in range(0, len(errorList)):
+        f.write(f"{errorList[i]}\n")
+
+with open(f'./outputs/training_output.txt', 'w') as f:
+    f.write(f"Initial Equation : {weights[1]}*s + {weights[2]}*g + {weights[3]}*e + {weights[4]}*r + {weights[5]}*c + {weights[0]} = 0\n\n")
+    for i in range(0, len(trainingOut_list)):
+        f.write(f"{trainingOut_list[i]}\n")
+    f.write(f"\nFinal Equation: {trainingOut_list[len(trainingOut_list)-1]}")
+
+
+# plotting graph
+
+# sns.set_theme(style="darkgrid")
+
+# # setting up dataframe
+# zipped = list(zip(errorList, epochs))
+# df = pd.DataFrame(zipped, columns=['Error Rate', 'Iterations'])
+
+# # More complicated, but more customizable appearance (seaborn)        
+# g = sns.relplot(x="Error Rate", y="Iterations", kind="line", data=df)
+# g.figure.autofmt_xdate()
+# plt.show()
+
+# #simple matplotlib
+# plt.plot(epochs, errorList)
+# plt.title('Error rate vs iterations')
+# plt.xlabel('Iterations')
+# plt.ylabel('Error rate (%)')
+# plt.show()
+
